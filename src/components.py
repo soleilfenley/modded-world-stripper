2ed4525ccf4811d1107019cd6678fd60f727231b
Soleil <60515000+flopsiedev@users.noreply.github.com>
2026-08-12 23:16:37 -0500

    Adding core cleaner. Created cleaner.py to do the actual tasks. Also made a components.py for reused tasks for reading and writing to nbt data.

---
+++ src/components.py
@@ -0,0 +1,17 @@
+
+import gzip
+import io
+from pathlib import Path
+
+import nbtlib
+
+
+def read_nbt(path: Path) -> nbtlib.File:
+        with open(path, "rb") as file:
+                return nbtlib.File.parse(io.BytesIO(gzip.decompress(file.read())))
+
+def write_nbt(path: Path, data: nbtlib.File) -> None:
+        buffer = io.BytesIO()
+        data.write(buffer)
+        with open(path, "wb") as file:
+                file.write(gzip.compress(buffer.getvalue()))
